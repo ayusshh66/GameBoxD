7b8792600636be759bf59e645439cdafb2f88ef5
ayusshh66 <adonisjefrey66@gmail.com>
2026-09-12 23:11:40 +0530

    made login user

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -3,6 +3,11 @@ import jwt from "jsonwebtoken";
 import { db, users } from "../../db";
 import {findUserByEmail, findUserById, createUser} from "./auth.repository"
 
+const JWT_SECRET = process.env.JWT_SECRET;
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET environment variable is missing.");
+}
+
 export const register = async(username : string,
     email : string,
     password : string,
@@ -27,5 +32,38 @@ export const register = async(username : string,
         throw error
     }
 
+}
+
+export const loginUser = async(email : string , password : string) =>{
+
+    try {
+        
+        const existingUser = await findUserByEmail(email);
+
+        if(!existingUser){
+            throw new Error("Invalid email or password");
+        }
+
+        const newHashPassword = await bcrypt.compare(password, existingUser.passwordHash);
+
+        if(!newHashPassword){
+            throw new Error("Invalid Password")
+        }
+
+        const token = jwt.sign({
+            userId : existingUser.id
+        },JWT_SECRET, {
+            expiresIn : "7d",
+        })
+
+        return {
+            user : existingUser.username,
+            token,
+        }
+        
+    } catch (error) {
+        console.error(`error in login`)
+        throw error;
+    }
 }
 
