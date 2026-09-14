091fe3bda8e62b095fd2244f5e6a3898e932100a
ayusshh66 <adonisjefrey66@gmail.com>
2026-09-15 00:03:48 +0530

    added the repository methods to the services file

--- backend/src/modules/games/games.service.ts
+++ backend/src/modules/games/games.service.ts
@@ -1,4 +1,4 @@
-import { findAllGames, findGameBySlug } from "./games.repository"
+import { findAllGames, findGameBySlug, findLatestGames, findTopGames, findUpcominigGames, searchGames } from "./games.repository"
 
 
 export const getAllGames = async(limit: number, offset:number) => {
@@ -10,5 +10,29 @@ export const getAllGames = async(limit: number, offset:number) => {
 export const getGameBySlug = async(slug:string) =>{
 
     return await findGameBySlug(slug);
+
+}
+
+export const getLatestGames = async(limit:number) => {
+
+    return await findLatestGames(limit);
+
+}
+
+export const getUpcomingGames = async(limit:number) => {
+
+    return await findUpcominigGames(limit);
+
+}
+
+export const getTopGames = async(limit:number) => {
+
+    return await findTopGames(limit);
+
+}
+
+export const getSearchTopGames = async(query:string, limit:number, offset:number) => {
+
+    return await searchGames(query,limit,offset);
 
 }
