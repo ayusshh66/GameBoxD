65cf403993f1e6b5115b9eab9abafbe999302589
ayusshh66 <adonisjefrey66@gmail.com>
2026-09-14 23:40:09 +0530

    made method to find latest games with limit

--- backend/src/modules/games/games.repository.ts
+++ backend/src/modules/games/games.repository.ts
@@ -13,7 +13,7 @@ export const findAllGames = async(limit : number,
             offset : offset,
             orderBy: (games, {desc}) => [desc(games.createdAt)],
         });
-        return result;
+        return result ?? null;
         
     } catch (error) {
         console.error("error in getting all the games", error);
@@ -34,6 +34,25 @@ export const findGameBySlug = async(slug : string) =>{
         
     } catch (error) {
         console.error("error in finding games my slug",error)
+        throw error;
+    }
+
+}
+
+export const findLatestGames = async(limit : number) => {
+
+    try {
+
+        const result = await db.query.games.findMany({
+            where : (games, {eq}) => eq(games.status, "released"),
+            limit : limit,
+            orderBy : (games, {desc}) => [desc(games.releaseDate)]
+        })
+
+        return result;
+        
+    } catch (error) {
+        console.error("error in fetching latest game", error);
         throw error;
     }
 
