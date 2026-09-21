3b1566e17ec9244e7e6b0b252386f76416a379ae
ayusshh66 <adonisjefrey66@gmail.com>
2026-09-21 23:37:52 +0530

    fixing bugs

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,16 +1,13 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as dotenv from "dotenv";
-import * as schema from "./schema/index";
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless"; // 👈 Note: neon-serverless instead of neon-http
+import ws from "ws";
+import * as schema from "./schema";
 
-dotenv.config();
+// Configure WebSockets for Neon
+neonConfig.webSocketConstructor = ws;
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable is missing.");
-}
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
 
-const sql = neon(process.env.DATABASE_URL);
-
-export const db = drizzle({ client: sql, schema });
+export const db = drizzle(pool, { schema });
 
 export * from "./schema/index";
