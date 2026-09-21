e9b1016e0c0ec371afe33222324fa1f1ba5ebfbe
ayusshh66 <adonisjefrey66@gmail.com>
2026-09-21 21:01:27 +0530

    made route for posting game

--- backend/src/modules/games/games.routes.ts
+++ backend/src/modules/games/games.routes.ts
@@ -7,7 +7,10 @@ import {
   upcomingGames,
   topGames,
   searchTopGames,
+  postGame,
 } from "./games.controller";
+import { authenticate } from "../auth/auth.middleware";
+import { authorize } from "../../db/middleware/authorize";
 
 const router = Router();
 
@@ -22,5 +25,13 @@ router.get("/top", topGames);
 router.get("/search", searchTopGames);
 
 router.get("/:slug", getGame);
+
+router.post(
+  "/",
+  authenticate,
+  authorize("admin", "moderator"),
+  postGame
+);
+
 
 export default router;  
