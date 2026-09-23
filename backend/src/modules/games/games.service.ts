90a2e3e9bd0828e99ad0a82bc58206b535d08ef6
ayusshh66 <adonisjefrey66@gmail.com>
2026-09-23 13:56:37 +0530

    made a method to update (patch) game details

--- backend/src/modules/games/games.service.ts
+++ backend/src/modules/games/games.service.ts
@@ -1,5 +1,6 @@
 import { findAllGames, findGameBySlug, findLatestGames, findTopGames, findUpcominigGames, searchGames } from "./games.repository"
 import { db, games, gameGenres, gamePlatforms, gameTags } from "../../db";
+import { eq } from "drizzle-orm";
 
 export const getAllGames = async(limit: number, page:number) => {
 
@@ -41,7 +42,7 @@ export const getSearchTopGames = async(query:string, limit:number, page:number) 
 
 }
 
-//in
+//inface
 interface CreateGameInput {
   name: string;
   slug: string;
@@ -56,7 +57,7 @@ interface CreateGameInput {
   genreIds: string[];
   platformIds: string[];
 }
-
+// create game use db.transaction
 export const createGame = async(data:CreateGameInput) => {
 
     // db.transaction = all or nothing
@@ -101,6 +102,126 @@ export const createGame = async(data:CreateGameInput) => {
         }
 
         return game;
+    })
+
+}
+// interface to udpdate games
+interface UpdateGameInput {
+  name?: string;
+  slug?: string;
+  description?: string;
+  coverUrl?: string;
+  backgroundUrl?: string;
+  releaseDate?: Date;
+  status?: "upcoming" | "released" | "cancelled";
+  metacriticScore?: number;
+
+  genreIds?: string[];
+  platformIds?: string[];
+  tagIds?: string[];
+}
+
+export const updateGame = async(
+    gameId : string,
+    data : UpdateGameInput,
+) => {
+
+    return db.transaction(async(tx) => {
+        //checks if game exisits with the provided gameId
+        const existingGame = await tx.query.games.findFirst({
+            where : (games, {eq}) => (eq(games.id, gameId)),
+        })
+
+        if (!existingGame) {
+            return null;
+        }
+
+        //if user dont give any field then it will be undefined and wont be updated
+        const gameData = {
+      ...(data.name !== undefined && {
+        name: data.name,
+      }),
+
+      ...(data.slug !== undefined && {
+        slug: data.slug,
+      }),
+
+      ...(data.description !== undefined && {
+        description: data.description,
+      }),
+
+      ...(data.coverUrl !== undefined && {
+        coverUrl: data.coverUrl,
+      }),
+
+      ...(data.backgroundUrl !== undefined && {
+        backgroundUrl: data.backgroundUrl,
+      }),
+
+      ...(data.releaseDate !== undefined && {
+        releaseDate: data.releaseDate,
+      }),
+
+      ...(data.status !== undefined && {
+        status: data.status,
+      }),
+
+      ...(data.metacriticScore !== undefined && {
+        metacriticScore: data.metacriticScore,
+      }),
+
+      updatedAt: new Date(),
+    };
+
+    if(Object.keys( gameData).length>1){
+        await tx.update(games).set(gameData).where(eq(games.id, gameId))
+    }
+
+    if(data.platformIds !== undefined){
+        await tx.delete(gamePlatforms).where(eq(gamePlatforms.gameId, gameId));
+
+        if(data.platformIds.length>0){
+            await tx.insert(gamePlatforms).values(
+                data.platformIds.map((platformId) => ({
+                    gameId,
+                    platformId,
+                }))
+            )
+        }
+    }
+
+    if(data.tagIds !== undefined){
+        await tx.delete(gameTags).where(eq(gameTags.gameId, gameId));
+
+        if(data.tagIds.length>0){
+            await tx.insert(gameTags).values(
+                data.tagIds.map((tagId) => ({
+                    gameId,
+                    tagId,
+                }))
+            )
+        }
+    }
+
+    if(data.genreIds !==undefined){
+        await tx.delete(gameGenres).where(eq(gameGenres.gameId, gameId));
+
+        if(data.genreIds.length > 0){
+            await tx.insert(gameGenres).values(
+                data.genreIds.map((genreId) => ({// we do "({})" when we dont want to use return  
+                    gameId,
+                    genreId,
+                }))
+            )
+        }
+    }
+
+    const updateGame = await tx.query.games.findFirst({
+        where : (games, {eq}) => (eq(games.id, gameId))
+    })
+
+    return updateGame;
+
     })
 
 }
