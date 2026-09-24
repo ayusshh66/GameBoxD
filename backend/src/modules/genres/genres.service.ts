b829bc9b04575812ff47349c86b49f9ee1d2cb76
ayusshh66 <adonisjefrey66@gmail.com>
2026-09-24 23:59:30 +0530

    making of genre service get all games by id

--- backend/src/modules/genres/genres.service.ts
+++ backend/src/modules/genres/genres.service.ts
@@ -1,8 +1,14 @@
-import { findAllGenres } from "./genres.repository"
+import { findAllGenres, findGenresById } from "./genres.repository"
 
 
 export const getAllGenres = async() => {
 
     return await findAllGenres();
+
+}
+
+export const getAllGenresById = async(genreId:string) => {
+
+    return await findGenresById(genreId);
 
 }
