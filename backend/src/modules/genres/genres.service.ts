f037479f3da742b8d7dbbc22bfdeca9a1a68052e
ayusshh66 <adonisjefrey66@gmail.com>
2026-09-25 00:04:29 +0530

    made a service to update genre

--- backend/src/modules/genres/genres.service.ts
+++ backend/src/modules/genres/genres.service.ts
@@ -1,5 +1,6 @@
+import { db } from "../../db";
 import { findAllGenres, findGenreBySlug, findGenresById, updateGenre, deleteGenre,createGenre } from "./genres.repository"
-import { CreateGenreInput } from "./genres.validation";
+import { CreateGenreInput, UpdateGenreInput } from "./genres.validation";
 
 
 export const getAllGenres = async() => {
@@ -23,5 +24,19 @@ export const getGenresBySlug = async(slug:string) => {
 export const createGenreService = async(data:CreateGenreInput) => {
 
     return await createGenre(data);
+
+}
+
+export const updateGenreService = async(genreId:string, data:UpdateGenreInput) => {
+
+    const existingGenre = await db.query.genres.findFirst({
+        where : (genres, {eq}) => (eq(genres.id, genreId)),
+    })
+
+    if(!existingGenre){
+        return null;
+    }
+
+    return await updateGenre(genreId,data);
 
 }
