8b09a3a03d49d963c45fe4ee60d995c93f704c2c
ayusshh66 <adonisjefrey66@gmail.com>
2026-09-25 00:27:20 +0530

    made controlle to find genre by slug

--- backend/src/modules/genres/genres.controller.ts
+++ backend/src/modules/genres/genres.controller.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
-import { getAllGenres, getAllGenresById } from "./genres.service";
+import { getAllGenres, getAllGenresById, getGenresBySlug } from "./genres.service";
 
 export const getAllGenresController = async (
   req: Request,
@@ -36,6 +36,28 @@ export const getAllGenreByIdController = async(req:Request<{genreId:string}>, re
         success:true,
         message:"genre found",
         data:genreById,
+    })
+
+}
+
+export const getAllGenreBySlugController = async(req:Request<{slug:string}>,res:Response, next:NextFunction) => {
+
+    const {slug} = req.params;
+
+    const genreBySlug = await getGenresBySlug(slug);
+
+    if(!genreBySlug){
+        return res.status(400).json({
+            success:false,
+            message:"no genre found with this slug"
+        })
+    }
+
+
+    return res.status(200).json({
+        success:true,
+        message:"genre found!",
+        data:genreBySlug,
     })
 
 }
